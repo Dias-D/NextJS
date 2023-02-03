36fa477d912804847efba000851cb77325fee066
Diego Dias <diasdsdiego@gmail.com>
2023-02-03 20:53:07 -0300

    missing image above o

--- src/pages/components/Header.jsx
+++ src/pages/components/Header.jsx
@@ -3,17 +3,18 @@ import Image from "next/image";
 function Header() {
     return (
         <>
-            <div className="flex justify-center my-6">
-                <Image src="/../public/images/logo.png" alt="logo" width="128" height="128" />
+            <div className="flex justify-center my-8">
+                <Image src="/../public/images/logo.png" alt="logo" width="180" height="180" />
             </div>
-            <div className="flex flex-col justify-center items-center bg-sky-50 w-full h-80">
-                <div>
-                    <button className="bg-transparent text-blue-600 font-medium py-2 px-4 border border-2 border-blue-500 customRounded">
-                        <span className="font-semibold">WEBINARS </span> EXCLUSIVOS
+            <div className="bg-sky-50 w-full">
+                <div className="flex flex-col justify-center items-center py-40">
+                    <button className="bg-transparent text-sm font-semibold py-1 px-4 border border-2 customRounded">
+                        <span className="font-bold">WEBINARS </span> EXCLUSIVOS
                     </button>
-                </div>
-                <div className="mt-4">
-                    <span className="text-6xl font-medium">Menos Conversinha,</span>
+                    <span className="mt-3 text-5xl-sm font-normal leading-none">Menos Conversinha,</span>
+                    <span className="text-7xl-sm font-bold leading-none customBlueSpanHeader">Mais Conversão</span>
+                    <hr class="w-5/12 h-1 mx-auto bg-gray-200 border-0 rounded md:my-6 dark:bg-gray-700" />
+                    <span className="text-llg leading-none">Conheça as estratégias que <strong>mudaram o jogo</strong> e como aplicá-las no seu negócio</span>
                 </div>
             </div>
         </>
