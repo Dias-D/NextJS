771e22c58b23922fe5ad47ad32878ab123461b0e
Diego Dias <diasdsdiego@gmail.com>
2023-02-09 13:20:09 -0300

    import images and modifify json

--- src/pages/api/library.js
+++ src/pages/api/library.js
@@ -9,21 +9,21 @@ const library = [
   {
     "id": 2,
     "type": "agency",
-    "title": "Como aumentar sua geração de Leads feat. Traktor",
+    "title": "Como aumentar as Leads feat. Traktor",
     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent scelerisque dui ut tellus vestibulum, id commodo tellus fringilla.",
     "export": ["spreadsheet", "document", "presentation", "folder"]
   },
   {
     "id": 3,
     "type": "agency",
-    "title": "Como aumentar sua geração de Leads feat. Traktor",
+    "title": "Como aumentar sua geração de Leads",
     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent scelerisque dui ut tellus vestibulum, id commodo tellus fringilla.",
     "export": ["spreadsheet", "document", "presentation", "folder"]
   },
   {
     "id": 4,
     "type": "agency",
-    "title": "Como aumentar sua geração de Leads feat. Traktor",
+    "title": "Como aumentar suas Leads feat.",
     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent scelerisque dui ut tellus vestibulum, id commodo tellus fringilla.",
     "export": ["spreadsheet", "document", "presentation", "folder"]
   },
@@ -44,14 +44,14 @@ const library = [
   {
     "id": 7,
     "type": "agency",
-    "title": "Como aumentar sua geração de Leads feat. Traktor",
+    "title": "Como aumentar as Leads feat. Traktor",
     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent scelerisque dui ut tellus vestibulum, id commodo tellus fringilla.",
     "export": ["spreadsheet", "document", "presentation", "folder"]
   },
   {
     "id": 8,
     "type": "agency",
-    "title": "Como aumentar sua geração de Leads feat. Traktor",
+    "title": "Como aumentar sua geração de Leads",
     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent scelerisque dui ut tellus vestibulum, id commodo tellus fringilla.",
     "export": ["spreadsheet", "document", "presentation", "folder"]
   },
