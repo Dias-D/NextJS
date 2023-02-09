0c248add63a67e1b3fd83ab6f0d4fd44eb1899b8
Diego Dias <diasdsdiego@gmail.com>
2023-02-09 13:20:26 -0300

    miport images

--- src/pages/components/Informative.jsx
+++ src/pages/components/Informative.jsx
@@ -1,4 +1,8 @@
 import Image from "next/image";
+import Comparative from "public/images/comparativo_img_CTA.png";
+import NoCard from "public/images/no-card-dark.webp";
+import Rating from "public/images/rating.webp";
+import Selo from "public/images/selo_RD.png";
 
 function Informative() {
     return (
@@ -6,7 +10,7 @@ function Informative() {
             <section className="flex flex-row bg-sky-50 w-full h-auto pt-12 pb-4">
                 <div className="flex flex-row m-auto">
                     <div className="customInformativeImage">
-                        <Image src="/../public/images/comparativo_img_CTA.png" alt="comparativo" layout="fill" objectFit="cover" />
+                        <Image src={ Comparative } alt="comparativo" fill  />
                     </div>
                     <div className="flex flex flex-col justify-center w-6/12 ml-16">
                         <span className="text-5xl py-1 text-neutral-800 font-semibold leading-none">Pronto para triplicar sua</span>
@@ -18,17 +22,17 @@ function Informative() {
                                 <span className="px-5 text-lg">VER DEMONSTRAÇÃO</span>
                             </button>
                             <div className="customInformativeSeloImage">
-                                <Image src="/../public/images/selo_RD.png" alt="selo RD" layout="fill" objectFit="cover" />
+                                <Image src={ Selo } alt="selo RD" fill  />
                             </div>
                         </div>
                         <div className="flex flex-row mt-2 items-center">
                             <div className="h-4 w-4 relative">
-                                <Image src="/../public/images/no-card-dark.webp" alt="selo RD" layout="fill" objectFit="cover" />
+                                <Image src={ NoCard } alt="selo RD" fill  />
                             </div>&nbsp;
                             <span className="text-neutral-600 font-bold">Não</span>&nbsp;
                             <span className="text-neutral-600">é necessário Cartão de Crédito |</span>&nbsp;                            
                             <div className="h-4 w-24 relative">
-                                <Image src="/../public/images/rating.webp" alt="selo RD" layout="fill" objectFit="cover" />
+                                <Image src={ Rating } alt="selo RD" fill  />
                             </div>&nbsp;
                             <span className="text-neutral-600 font-bold">4.9</span>&nbsp;
                             <span className="text-neutral-600">/5 média de satiwsfação</span>
